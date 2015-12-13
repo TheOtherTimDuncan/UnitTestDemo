ea6fc694efa90b0e8b98fb1118082ae0def86ebe
Tim <tduncan72@gmail.com>
2015-12-13 17:27:11 -0500

    Rename IQueryHandler methods

--- Demo.Web/Domain/Contracts/Queries/QueryHandler.cs
+++ Demo.Web/Domain/Contracts/Queries/QueryHandler.cs
@@ -6,11 +6,11 @@ namespace Demo.Web.Domain.Contracts.Queries
 {
     public interface IQueryHandler<in TParameter, out TResult> where TParameter : IQuery<TResult>
     {
-        TResult Retrieve(TParameter query);
+        TResult Execute(TParameter query);
     }
 
     public interface IAsyncQueryHandler<in TParameter, TResult> where TParameter : IAsyncQuery<TResult>
     {
-        Task<TResult> RetrieveAsync(TParameter query);
+        Task<TResult> ExecuteAsync(TParameter query);
     }
 }
