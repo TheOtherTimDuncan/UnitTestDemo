b3cbd8bd07fd5b6e0fbef2877b5fbc995c2a26e8
Tim <tduncan72@gmail.com>
2015-12-13 11:13:48 -0500

    Add Calculator class with tests based on MsTest

---
+++ Demo.Test.MsTest/CalculatorTests.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Demo.Test.MsTest
+{
+    [TestClass]
+    public class CalculatorTests
+    {
+        [TestClass]
+        public class AddMethodTests : CalculatorTests
+        {
+            [TestMethod]
+            public void ReturnsSumOfGivenValues()
+            {
+                Calculator calculator = new Calculator();
+                int result = calculator.Add(1, 2, 3);
+                Assert.AreEqual(6, result, "that's how math works");
+            }
+
+            [TestMethod]
+            [ExpectedException(typeof(ArgumentOutOfRangeException))]
+            public void ThrowsExceptionIfNoValuesGiven()
+            {
+                Calculator calculator = new Calculator();
+                int result = calculator.Add();
+            }
+
+            [TestMethod]
+            [ExpectedException(typeof(ArgumentOutOfRangeException))]
+            public void ThrowsExceptionIfOnlyOneValueGiven()
+            {
+                Calculator calculator = new Calculator();
+                int result = calculator.Add(1);
+            }
+        }
+
+        [TestClass]
+        public class SubtractMethodTests : CalculatorTests
+        {
+            [TestMethod]
+            public void ReturnsResultOfValuesSubtractedFromEachOther()
+            {
+                Calculator calculator = new Calculator();
+                int result = calculator.Subtract(3, 2, 1);
+                Assert.AreEqual(0, result, "that's how math works");
+            }
+
+            [TestMethod]
+            [ExpectedException(typeof(ArgumentOutOfRangeException))]
+            public void ThrowsExceptionIfNoValuesGiven()
+            {
+                Calculator calculator = new Calculator();
+                int result = calculator.Subtract();
+            }
+
+            [TestMethod]
+            [ExpectedException(typeof(ArgumentOutOfRangeException))]
+            public void ThrowsExceptionIfOnlyOneValueGiven()
+            {
+                Calculator calculator = new Calculator();
+                int result = calculator.Subtract(1);
+            }
+        }
+
+        [TestClass]
+        public class MultiplyMethodTests : CalculatorTests
+        {
+            [TestMethod]
+            public void ReturnsResultOfValuesMultipliedTogether()
+            {
+                Calculator calculator = new Calculator();
+                int result = calculator.Multiply(2, 3, 4);
+                //Assert.AreEqual(12, result, "that's how math works");
+                Assert.AreEqual(24, result, "that's how math works");
+            }
+
+            [TestMethod]
+            [ExpectedException(typeof(ArgumentOutOfRangeException))]
+            public void ThrowsExceptionIfNoValuesGiven()
+            {
+                Calculator calculator = new Calculator();
+                int result = calculator.Multiply();
+            }
+
+            [TestMethod]
+            [ExpectedException(typeof(ArgumentOutOfRangeException))]
+            public void ThrowsExceptionIfOnlyOneValueGiven()
+            {
+                Calculator calculator = new Calculator();
+                int result = calculator.Multiply(1);
+            }
+        }
+
+        [TestClass]
+        public class DivideMethodTests : CalculatorTests
+        {
+            [TestMethod]
+            public void ReturnsResultOfValuesDividedAgainstEach()
+            {
+                Calculator calculator = new Calculator();
+                decimal result = calculator.Divide(24, 4, 2);
+                Assert.AreEqual(3, result, "that's how math works");
+            }
+
+            [TestMethod]
+            [ExpectedException(typeof(ArgumentOutOfRangeException))]
+            public void ThrowsExceptionIfNoValuesGiven()
+            {
+                Calculator calculator = new Calculator();
+                decimal result = calculator.Divide();
+            }
+
+            [TestMethod]
+            [ExpectedException(typeof(ArgumentOutOfRangeException))]
+            public void ThrowsExceptionIfOnlyOneValueGiven()
+            {
+                Calculator calculator = new Calculator();
+                decimal result = calculator.Divide(1);
+            }
+        }
+    }
+}
---
+++ Demo/Calculator.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Demo
+{
+    public class Calculator
+    {
+        public int Add(params int[] values)
+        {
+            if (values.Length <= 1)
+            {
+                throw new ArgumentOutOfRangeException("values", "At least two values are required");
+            }
+
+            return values.Sum();
+        }
+
+        public int Subtract(params int[] values)
+        {
+            if (values.Length <= 1)
+            {
+                throw new ArgumentOutOfRangeException("values", "At least two values are required");
+            }
+
+            //return values[0] - values.Skip(1).Sum(x => x * -1);
+            return values[0] + values.Skip(1).Sum(x => x * -1);
+        }
+
+        public int Multiply(params int[] values)
+        {
+            if (values.Length <= 1)
+            {
+                throw new ArgumentOutOfRangeException("values", "At least two values are required");
+            }
+
+            int result = 1;
+            foreach (int v in values)
+            {
+                result *= v;
+            }
+            return result;
+        }
+
+        public decimal Divide(params int[] values)
+        {
+            if (values.Length <= 1)
+            {
+                throw new ArgumentOutOfRangeException("values", "At least two values are required");
+            }
+
+            decimal result = values[0];
+            for (int i = 1; i < values.Length; i++)
+            {
+                result = result / values[i];
+            }
+            return result;
+        }
+    }
+}
