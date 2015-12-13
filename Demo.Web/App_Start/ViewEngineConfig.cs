397fae5c053a3a8b0562f24e4e0cb4070519ae84
Tim <tduncan72@gmail.com>
2015-12-13 12:36:34 -0500

    Add MVC5 web app

---
+++ Demo.Web/App_Start/ViewEngineConfig.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Mvc;
+
+namespace Demo.Web
+{
+    public static class ViewEngineConfig
+    {
+        public static void ConfigureViewEngines(ViewEngineCollection viewEngines)
+        {
+            // {0} = view name or layout name
+            // {1} = controller name
+            // {2} = area name
+
+            string[] locations = new string[]
+            {
+                "~/Sections/{1}/Views/{0}.cshtml",
+                "~/Sections/{1}s/Views/{0}.cshtml",
+                "~/Sections/Common/Views/{0}.cshtml"
+            };
+
+            string[] areaLocations = new string[]
+            {
+                "~/Sections/{2}/{1}/Views/{0}.cshtml",
+                "~/Sections/{2}/{1}s/Views/{0}.cshtml",
+                "~/Sections/{2}/Views/{0}.cshtml"
+            };
+
+            viewEngines.Clear();
+            viewEngines.Add(new RazorViewEngine()
+            {
+                ViewLocationFormats = locations,
+                MasterLocationFormats = locations,
+                PartialViewLocationFormats = locations,
+
+                AreaMasterLocationFormats = areaLocations,
+                AreaPartialViewLocationFormats = areaLocations,
+                AreaViewLocationFormats = areaLocations
+            });
+        }
+    }
+
+}
---
+++ Demo.Web/Sections/Home/HomeController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Mvc;
+
+namespace Demo.Web.Sections.Home
+{
+    [Route(HomeController.Route + "/{action}")]
+    public class HomeController : Controller
+    {
+        public const string Route = "Home";
+
+        [Route("~/")]
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
